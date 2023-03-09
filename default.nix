9d10a2997fb8378009560e729885798804d8ee4a
Marek Fajkus <marek.faj@gmail.com>
2023-03-09 22:45:00 +0100

    add source tar ball generation (#86)
    This is the low effort automated way towards AGPL complience.
    
    Simply we include source packed as a downloadable tar ball during the
    build of website. This tar ball doesn't include any files that could be
    generated by source (PDFs, build html etc) and **doesn't include .git**.
    So it provides completely anonymized view of source code without any
    history. It's also build in sandbox by user called `homeless-shelter`
    (which nix uses to build) with time set to unix timestamp 0 without
    connection to internet without access to host system ENV so it can't
    leak any metadata or personal info from machine it's being build on even
    if some code would attempt to inject such information.
    
    This is the easiest and most defensive way to satisfy
    https://github.com/ictunion/main-website/issues/84 I can think about.

--- default.nix
+++ default.nix
@@ -6,12 +6,13 @@
 , tex
 , nodeDependencies
 , nix-gitignore
+, gnutar
 }:
 stdenv.mkDerivation {
   name = "ict-union-website";
   version = "0.1.0";
   src = nix-gitignore.gitignoreSource [] ./.;
-  buildInputs = [ hugo dart-sass-embedded-bin pandoc ibm-plex tex ];
+  buildInputs = [ hugo dart-sass-embedded-bin pandoc ibm-plex tex gnutar ];
   buildPhase = ''
     OSFONTDIR=${ibm-plex}/share/fonts/opentype bash latex/print-all
 
@@ -21,5 +22,9 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/var/www
     mv public/* $out/var/www
+
+    # Note that .git is not included in build source
+    tar -czvf source.tar.gz -C $src .
+    mv source.tar.gz $out/var/www/downloads/
   '';
 }
