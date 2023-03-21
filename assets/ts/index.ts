dbaad52afe09111f84e24e06a77340729bedb1d5
Marek Fajkus <marek.faj@gmail.com>
2023-03-21 22:44:01 +0100

    Highlight occupations (#133)
    closes #57
    
    - introduces data file for occupations (yaml since in this case yaml has
    nicer formatting)
    - implements logic for rotating though occupations on home page
    - updates FAQ concerning occupations to use new data source
    
    ----------
    __Please confirm the following:__
    
    - [x] I accept and respect the [statuses of Odborová organizace
    pracujících v
    ICT](https://ictunion.cz/downloads/ictunion-statutes-en.pdf)
    - [x] I'm providing all content changes under the [CC BY
    4.0](https://creativecommons.org/licenses/by/4.0/) license

---
+++ assets/ts/index.ts
@@ -0,0 +1,54 @@
+function shuffle<T>(a: T[]): T[] {
+    var j, x, i: number;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+
+const typingElements = document.querySelectorAll('[data-effect="shuffled-typing"]');
+
+typingElements.forEach((typing: HTMLElement) => {
+    // oh well.. double json parse because for some reason hugo feels stringifies JSON implicitelly
+    let occupations: string[] = shuffle(JSON.parse(JSON.parse(typing.querySelector('script').innerText)));
+    let occupation_index = 0;
+    let index = 0;
+    let occupation: string = occupations[occupation_index];
+
+    function nextOccupation() {
+        occupation_index++;
+        if (occupation_index < occupations.length) {
+            occupation = occupations[occupation_index];
+        } else {
+            occupation_index = 0;
+            occupation = occupations[0];
+        }
+    }
+
+    function typeLetter() {
+        if (index < occupation.length) {
+            typing.innerText += occupation.charAt(index);
+            window.setTimeout(typeLetter, 100);
+            index++;
+        } else {
+            window.setTimeout(backspace, 100 * occupation.length);
+        }
+    }
+
+    function backspace() {
+        if (index >= 0) {
+            typing.innerText = typing.innerText.slice(0, -1);
+            window.setTimeout(backspace, 20);
+            index--;
+        } else {
+            nextOccupation();
+            window.setTimeout(typeLetter, 200);
+        }
+    }
+
+    window.setTimeout(typeLetter, 200);
+
+});
--- content/faq/12-who-are-we.cs.md
+++ content/faq/12-who-are-we.cs.md
@@ -1,4 +1,7 @@
 ---
 title: Jaké profese můžou být členové*ky vašich odborů?
 ---
-**Členem nebo členkou našich odborů se může stát kdokoliv, kdo pracuje v IT sektoru.** Nezáleží na tom, co přesně děláte. Naši členové a členky jsou například programátorky, testeři, designérky, servisní technici, specialistky technické podpory, datoví analytici, správkyně sítí, databázoví analytici, webové vývojářky, systémoví analytici, IT konzultantky, softwaroví analytici, technici datových center, systémoví správci, business analytičky, síťové inženýrky, správci databází, systémové inženýrky, hardwaroví inženýři, frontendoví vývojáři, specialistky na kyberbezpečnost, datoví vědci, AI vývojářky, herní vývojáři, analytičky rizik, kontroloři kvality, UX designérky, operátoři call centra, technici help desku, inženýři cloudu, devops specialistky, freelanceři, stážisti, studentky atd.
+**Členem nebo členkou našich odborů se může stát kdokoliv, kdo pracuje v IT sektoru.**
+Nezáleží na tom, co přesně děláte. Naši členové a členky jsou například:
+
+{{< occupations >}}
