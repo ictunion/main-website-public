2e6c7a5c78c9f5aff98161dfb46a0ae52055feb5
Kubik161 <30665071+Kubik161@users.noreply.github.com>
2026-03-16 08:25:35 +0100

    Actionnetwork survey integration (#346)
    Closes #345 
    
    
    ----------
    __Please confirm the following:__
    
    - [x] I accept and respect the [statuses of Odborová organizace
    pracujících v
    ICT](https://ictunion.cz/downloads/ictunion-statutes-en.pdf)
    - [x] I'm providing all content changes under the [CC BY
    4.0](https://creativecommons.org/licenses/by/4.0/) license

--- assets/ts/events.ts
+++ assets/ts/events.ts
@@ -1,7 +1,9 @@
 import { XMap } from './web-component/XMap';
 import { Xqr } from './web-component/Xqr';
 import { XPetition } from './web-component/XPetition';
+import { XSurvey } from './web-component/XSurvey';
 
 customElements.define('x-map', XMap);
 customElements.define('x-qr', Xqr);
 customElements.define('x-petition', XPetition);
+customElements.define('x-survey', XSurvey);
--- assets/ts/web-component/XSurvey.ts
+++ assets/ts/web-component/XSurvey.ts
@@ -1,26 +1,26 @@
-function loadJs(petitionId: string): Promise<Event> {
+function loadJs(surveyId: string): Promise<Event> {
     const script = document.createElement('script');
     return new Promise((resolve, reject) => {
         script.onload = resolve;
         script.onerror = reject;
 
-        script.src = `https://actionnetwork.org/widgets/v6/petition/${petitionId}?format=js&source=widget`;
+        script.src = `https://actionnetwork.org/widgets/v6/survey/${surveyId}?format=js&source=widget`;
         document.head.appendChild(script);
     });
 }
 
-export class XPetition extends HTMLElement {
+export class XSurvey extends HTMLElement {
     public connectedCallback() {
         const button: HTMLButtonElement = this.querySelector('[role=allow]');
-        button.addEventListener('click', this.insertPetition.bind(this));
+        button.addEventListener('click', this.insertSurvey.bind(this));
     }
 
-    private async insertPetition() {
-        const petitionId: string = this.getAttribute('petitionId');
+    private async insertSurvey() {
+        const surveyId: string = this.getAttribute('surveyId');
         const container = document.createElement('div');
-        container.id = `can-petition-area-${petitionId}`;
+        container.id = `can-survey-area-${surveyId}`;
         container.classList.add('event-detail-petition-embed');
         this.replaceChildren(container);
-        await loadJs(petitionId);
+        await loadJs(surveyId);
     }
 }
